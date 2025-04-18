a407b5482d24f5d6c6dfa3bba29d4bc861f85325
mahmud khan <mahmudinfo85@gmail.com>
2025-04-19 03:36:25 +0530

    Multiple Pages Serve in node js without express js

--- app.js
+++ app.js
@@ -2,26 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const serverFile = (res, filename, contentType) => {
+    const filePath = path.join(__dirname, filename);
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error Loading Files');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+}
+
 const server = http.createServer((req, res) => {
     if (req.url === '/' || req.url === '/index.html') {
-        const filePath = path.join(__dirname, 'index.html');
-
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.writeHead(500);
-                res.end('Error loading html file');
-            } else {
-                res.writeHead(200, { "content-type": 'text/html' });
-                res.end(data);
-            }
-        })
+        serverFile(res, 'index.html', 'text/html');
+    } else if (req.url === '/about.html') {
+        serverFile(res, 'about.html', 'text/html');
+    } else if (req.url === '/contact.html') {
+        serverFile(res, 'contact.html', 'text/html');
     } else {
-        res.writeHead(404, { 'content-type': 'text/plain' });
-        res.end('404 ppage not found');
-
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page Not Found');
     }
-})
+});
 
 server.listen(3000, () => {
-    console.log('Server Running at localhost:3000');
-})
+    console.log('Server running at http://localhost:3000');
+});
