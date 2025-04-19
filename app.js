7807c5e750ecc8822d8b849d0651aeff318c87be
mahmud khan <mahmudinfo85@gmail.com>
2025-04-19 14:39:50 +0530

    add css file

--- app.js
+++ app.js
@@ -2,31 +2,41 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const serverFile = (res, filename, contentType) => {
+const serveFile = (res, filename, contentType) => {
     const filePath = path.join(__dirname, filename);
     fs.readFile(filePath, (err, data) => {
         if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error Loading Files');
+            res.writeHead(500, { 'content-type': 'text/plain' });
+            res.end('error loading file');
         } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(data);
+            res.writeHead(200, { 'content-type': contentType });
+            res.end(data)
         }
-    });
+    })
 }
 
 const server = http.createServer((req, res) => {
     if (req.url === '/' || req.url === '/index.html') {
-        serverFile(res, 'index.html', 'text/html');
-    } else if (req.url === '/about.html') {
-        serverFile(res, 'about.html', 'text/html');
+        serveFile(res, 'index.html', 'text/html');
     } else if (req.url === '/contact.html') {
-        serverFile(res, 'contact.html', 'text/html');
+        serveFile(res, 'contact.html', 'text/html');
+    } else if (req.url === '/about.html') {
+        serveFile(res, 'about.html', 'text/html');
+    } else if (req.url.startsWith('/public/')) {
+        const filePath = req.url.slice(1);
+        const ext = path.extname(filePath);
+
+        if (ext === '.css') {
+            serveFile(res, filePath, 'text/css');
+        } else {
+            res.writeHead(403, { 'content-type': 'text/plain' });
+            res.end('forbiden file type')
+        }
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Page Not Found');
+        res.end('404 - Page Not Found');
     }
-});
+})
 
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000');
